c249763d88994adc082b265d09ad0fefb65666ad
JuanK <camiloalzatebedoya15@gmail.com>
2025-06-11 21:02:54 -0500

    New EndPoint for the project
    this endpoint return hello world and url to the endpoint

---
+++ src/main/java/com/example/demo/controllers/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String index() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Las Url de los endpoints son: ");
+        sb.append("http://localhost:8080/hello");
+        //sb.append("\nhttp://localhost:8080/");
+        return sb.toString();
+    }
+    
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
